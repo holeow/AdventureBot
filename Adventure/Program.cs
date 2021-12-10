e745445f4506d0cd52196432c063493f0c2fa700
hlw <holeow@gmx.fr>
2021-12-10 15:56:07 +0100

    cleanup

--- Adventure/Program.cs
+++ Adventure/Program.cs
@@ -25,23 +25,17 @@ namespace Adventure
             Client.SlashCommandExecuted += OnReceiveSlashCommand;
 
             
-            //  You can assign your bot token to a string, and pass that in to connect.
-            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            
             var token = FileManager.SecretConfig["token"];
 
             
-            //"TEST_GUILD_ID": "916380934406811738"
-            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-            // var token = File.ReadAllText("token.txt");
-            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            
 
             await Client.LoginAsync(TokenType.Bot, token);
             await Client.StartAsync();
 
 
-            //FileManager.Config["DevGuildID"] = 916380934406811738.ToString();
-            //FileManager.Config.Save();
+            
 
             // Block this task until the program is closed.
             await Task.Delay(-1);
